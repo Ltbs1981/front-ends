bbe88de20d80dadb208e990f4b5d3001e3e50f41
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-05-29 00:29:42 -0300

    Projeto inicial, imprimindo o nome na tela

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+    const nome = "Luciano Tadeu";
+    const sobrenome = "Thykkanno";
+
+  return (
+    <div>
+      <p>{nome} {sobrenome}</p>
+    </div>
+  );
+}
+
+export default App;
